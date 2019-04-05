2834b6a0096842ea241e371de037b0f396549fa9
Leonardo Rodrigues Miranda Lins <44787835+leonardormlins@users.noreply.github.com>
2019-04-05 20:53:20 -0300

    Update MainServer.java

--- RESTFul_Java/src/main/java/hello/MainServer.java
+++ RESTFul_Java/src/main/java/hello/MainServer.java
@@ -25,13 +25,9 @@ public class MainServer {
 		//Servir conteudo html, css e javascript
 		staticFileLocation("/static");
 
-		inicializarCarros();
 		inicializarAlunos();
 		Controller controller = new Controller(model);
 		
-		//controller.buscarCarro();
-		//controller.buscarCarroPlaca();
-		//controller.buscarCarroModelo();
 		controller.mostrarAulas();
 		controller.loginUser();
 		
@@ -39,18 +35,12 @@ public class MainServer {
     
    
     
-    public static void inicializarCarros(){
-    	model.addCarro(new Carro("AAA-1111", new Especificacao("gol", "vw", "verde")));
-    	
-    	model.addCarro(new Carro("BBB-1111", new Especificacao("gol", "vw", "verde")));
-    	model.addUser(new User("C@A.com","C",1));
-    }
-    
     //falta inicializar o aluno
     public static void inicializarAlunos() {
     	model.addAluno(new Aluno(1, "Jess", "Rua 1", "C@A.com", "123", "123", 1, null, null, new LinkedList<Aula>()));
     	model.addAula(new Aula (null, "Zumba", "segunda", "50min", null));
     	model.inclAula("Jess", "Zumba");
+	model.addUser(new User("C@A.com","C",1));
     	
     }
 }
