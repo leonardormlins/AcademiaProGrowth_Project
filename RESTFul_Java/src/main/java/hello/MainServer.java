a15a45883fa60c885a85b2b981801fbf62d7dc87
marcelofsteixeira <48607320+marcelofsteixeira@users.noreply.github.com>
2019-04-12 22:16:45 -0300

    Update MainServer.java

--- RESTFul_Java/src/main/java/hello/MainServer.java
+++ RESTFul_Java/src/main/java/hello/MainServer.java
@@ -37,11 +37,11 @@ public class MainServer {
     
    
     
-    //falta inicializar o aluno
+    
     public static void inicializarAlunos() {
     	model.addAluno(new Aluno(1, "Jess", "Rua 1", "C@A.com", "123", "123", 1, null, null, new LinkedList<Aula>()));
     	model.addAula(new Aula (null, "Zumba", "segunda", "50min", new Modalidade (01,"Zumba")));
-    	model.addAula(new Aula (null, "Boxe", "terça", "50min",  new Modalidade (02,"Boxe")));
+    	model.addAula(new Aula (null, "Boxe", "terÃ§a", "50min",  new Modalidade (02,"Boxe")));
     	model.addAula(new Aula (null, "Spinning", "Quarta", "50min", new Modalidade (03,"Spinning")));
 
     	model.inclAula("Jess", "Zumba");
