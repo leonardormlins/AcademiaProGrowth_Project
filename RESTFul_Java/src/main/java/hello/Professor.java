d19b03510c3fc9a18fde599b831531267ae986c3
Jéssica Helen <37513776+jesscahelen@users.noreply.github.com>
2019-06-07 21:47:11 -0300

    Update Professor.java

--- RESTFul_Java/src/main/java/hello/Professor.java
+++ RESTFul_Java/src/main/java/hello/Professor.java
@@ -10,7 +10,7 @@ public class Professor extends Funcionario {
 	public Professor(int id, String nome, String adress, String email, String telefone, String expediente,
 			String descricaoCargo, String senha, char genero, int tipo) {
 		super(id, nome, adress, email, telefone, expediente, descricaoCargo, senha, genero, tipo);
-		this.aula = new LinkedList<Aula>();
+		
 	}
 	
 
